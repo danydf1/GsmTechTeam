1c21402312356c5b4cac9d811dc536b5eecdff7f
danydf1 <reitmatch@hotmail.com>
2021-09-24 13:06:12 -0300

    ya esta el color de fondo

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(130,window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(250,380);
 camera.position.setZ(10);
 
 renderer.render(scene,camera);
 
-const geometry = new THREE.TorusGeometry(10,3,16,100)
+const geometry = new THREE.TorusGeometry(10,3,16,70)
 const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
 const torus = new THREE.Mesh(geometry,material);
 
