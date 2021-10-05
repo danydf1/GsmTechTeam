5d83480d1fe3a36e0432a7fe346326327aa2cf4d
danydf1 <reitmatch@hotmail.com>
2021-10-05 16:19:01 -0300

    mobile casi terminado

--- index.js
+++ index.js
@@ -1,80 +1,87 @@
-// gsap.registerPlugin(ScrollTrigger);
-
-// var tl = gsap.timeline({
-//     scrollTrigger:{
-//        trigger:'#slider'
-//     }
-// })
-
-// tl.to('#mundo',{
-//     duration: 2,
-//     y:-500,
-//     opacity:0  
-// });
-
-// tl.to('#monitor',{
-//     start:300,
-//     opacity:1,
-//     y:-300,
-//     duration:2
-// })
-
-// tl.to('#monitor',{
-//     opacity:0
-// })
-// tl.to('#telefono',{
-//     opacity:1,
-//     y:0,
-//     duration:2
-// })
-
-// var slider = 0;
-
-// window.onscroll = function() {
-    
-//     var y = window.scrollY;   
-
-//     if(y >= 102 && slider == 0 )
-//     {
-//         desparece()        
-        
-//     }
-//     if(y >= 102 && y<164 && slider == 1)
-//     {
-//         document.getElementById("monitor").style = "block"
-               
-//         window.scroll(0,0)
-//     }
-//     if(y >= 102 && slider == 2){
-//         despareceMonitor()
-//     }
-
-//   };
-
-//  function desparece(){
-//     ImagenPrincpal = document.getElementById("ImagenPrincipal");
-//     ImagenPrincpal.classList.add("slide-out-right");
-    
-//     TituloPrincpal = document.getElementById("tituloMundo");
-//     TituloPrincpal.classList.add("fade-out");
-
-//      tituloNosotros = document.getElementById("tituloNos");
-//      tituloNosotros.className = "fade-out";
-     
-//       staffing = document.getElementById("tituloStaffing");
-//       staffing.classList.add("fade-out");
-//       slider = 1
-      
-//   } 
-
-//   function despareceMonitor(){
-//     frasemoni = document.getElementById("ciber");
-//     frasemoni.classList.remove("slide-in-left");
-//     frasemoni.classList.add("fade-out");
-// }
-
-
-document.getElementById("btn").addEventListener('click',()=> {
-    
-    document.getElementById("texto").style.display ="block"
+///los esto documents le dan accion a los mas del info
+/*CIBERSEGURIDAD BOTONES +*/
+//1
+document.getElementById("btnCiber1Abrir").addEventListener('click',()=> {
+    document.getElementById("texto1").style.display ="block"
+    document.getElementById("btnCiber1Cerrar").style.display = "inline"
+    document.getElementById("btnCiber1Abrir").style.display ="none"
 })
+document.getElementById("btnCiber1Cerrar").addEventListener('click',()=> {
+    document.getElementById("texto1").style.display ="none"
+    document.getElementById("btnCiber1Cerrar").style.display = "none"
+    document.getElementById("btnCiber1Abrir").style.display ="inline"})
+//2
+document.getElementById("btnCiber2Abrir").addEventListener('click',()=> {
+    document.getElementById("texto2").style.display ="block"
+    document.getElementById("btnCiber2Cerrar").style.display = "inline"
+    document.getElementById("btnCiber2Abrir").style.display ="none"
+})
+document.getElementById("btnCiber2Cerrar").addEventListener('click',()=> {
+    document.getElementById("texto2").style.display ="none"
+    document.getElementById("btnCiber2Cerrar").style.display = "none"
+    document.getElementById("btnCiber2Abrir").style.display ="inline"})
+//3
+document.getElementById("btnCiber3Abrir").addEventListener('click',()=> {
+    document.getElementById("texto3").style.display ="block"
+    document.getElementById("btnCiber3Cerrar").style.display = "inline"
+    document.getElementById("btnCiber3Abrir").style.display ="none"
+})
+document.getElementById("btnCiber3Cerrar").addEventListener('click',()=> {
+    document.getElementById("texto3").style.display ="none"
+    document.getElementById("btnCiber3Cerrar").style.display = "none"
+    document.getElementById("btnCiber3Abrir").style.display ="inline"}) 
+//4
+document.getElementById("btnCiber4Abrir").addEventListener('click',()=> {
+    document.getElementById("texto4").style.display ="block"
+    document.getElementById("btnCiber4Cerrar").style.display = "inline"
+    document.getElementById("btnCiber4Abrir").style.display ="none"
+})
+document.getElementById("btnCiber4Cerrar").addEventListener('click',()=> {
+    document.getElementById("texto4").style.display ="none"
+    document.getElementById("btnCiber4Cerrar").style.display = "none"
+    document.getElementById("btnCiber4Abrir").style.display ="inline"})   
+
+//--------------------------------------------------
+/*DESARROLLO BOTONES +*/
+//1
+document.getElementById("btnDesarrollo1Abrir").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo1").style.display ="block"
+    document.getElementById("btnDesarrollo1Cerrar").style.display = "inline"
+    document.getElementById("btnDesarrollo1Abrir").style.display ="none"
+})
+document.getElementById("btnDesarrollo1Cerrar").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo1").style.display ="none"
+    document.getElementById("btnDesarrollo1Cerrar").style.display = "none"
+    document.getElementById("btnDesarrollo1Abrir").style.display ="inline"})
+// //2
+document.getElementById("btnDesarrollo2Abrir").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo2").style.display ="block"
+    document.getElementById("btnDesarrollo2Cerrar").style.display = "inline"
+    document.getElementById("btnDesarrollo2Abrir").style.display ="none"
+})
+document.getElementById("btnDesarrollo2Cerrar").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo2").style.display ="none"
+    document.getElementById("btnDesarrollo2Cerrar").style.display = "none"
+    document.getElementById("btnDesarrollo2Abrir").style.display ="inline"})
+// //3
+document.getElementById("btnDesarrollo3Abrir").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo3").style.display ="block"
+    document.getElementById("btnDesarrollo3Cerrar").style.display = "inline"
+    document.getElementById("btnDesarrollo3Abrir").style.display ="none"
+})
+document.getElementById("btnDesarrollo3Cerrar").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo3").style.display ="none"
+    document.getElementById("btnDesarrollo3Cerrar").style.display = "none"
+    document.getElementById("btnDesarrollo3Abrir").style.display ="inline"}) 
+// //4
+document.getElementById("btnDesarrollo4Abrir").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo4").style.display ="block"
+    document.getElementById("btnDesarrollo4Cerrar").style.display = "inline"
+    document.getElementById("btnDesarrollo4Abrir").style.display ="none"
+})
+document.getElementById("btnDesarrollo4Cerrar").addEventListener('click',()=> {
+    document.getElementById("textoDesarrollo4").style.display ="none"
+    document.getElementById("btnDesarrollo4Cerrar").style.display = "none"
+    document.getElementById("btnDesarrollo4Abrir").style.display ="inline"})  
+
+//--------------------------------------------------
